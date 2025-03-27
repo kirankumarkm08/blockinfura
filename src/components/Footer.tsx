4608370f019dcdde86b5d58bbc694608224a2496
kiran kumar km <kirankumarkm1505@gmail.com>
2025-03-27 14:49:00 +0530

    updated vite config

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -181,7 +181,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400">
-            © 2024 BlockInfura. All rights reserved.
+            © 2025 BlockInfura. All rights reserved.
           </p>
         </div>
       </div>
